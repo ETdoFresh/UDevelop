2dbecdb05a37ba7f54e2c99d793af01a22809763
etdofresh@gmail.com <>
2023-10-09 18:02:14 -0500

    Create a popup open close behavior and add to a few ui components.

---
+++ Assets/UDevelop/UI/Behaviours/UIPopupCloseButtonBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using ETdoFresh.UnityPackages.EventBusSystem;
+using ETdoFresh.UnityPackages.ExtensionMethods;
+using UDevelop.UI.Events;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UDevelop.UI.Behaviours
+{
+    public class UIPopupCloseButtonBehaviour : MonoBehaviour
+    {
+        [SerializeField] private string popupName;
+        [SerializeField] private Button button;
+        
+        private void OnValidate()
+        {
+            if (string.IsNullOrEmpty(popupName)) popupName = gameObject.name;
+            if (!button) button = GetComponent<Button>();
+        }
+
+        private void OnEnable()
+        {
+            button.onClick.AddPersistentListener(OnButtonClick);
+        }
+        
+        private void OnDisable()
+        {
+            button.onClick.RemovePersistentListener(OnButtonClick);
+        }
+        
+        private void OnButtonClick()
+        {
+            EventBus.Invoke(new UIPopupEvent { PopupName = popupName, PopupType = UIPopupEvent.Type.Hide });
+        }
+    }
+}
---
+++ Assets/UDevelop/UI/Events/UIPopupEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace UDevelop.UI.Events
+{
+    public class UIPopupEvent : UIEvent
+    {
+        public enum Type { Hide, Show }
+
+        public string PopupName { get; set; }
+        public Type PopupType { get; set; } = Type.Show;
+    }
+}
