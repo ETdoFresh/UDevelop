a55a981f97ae87b67682cf73907e19ba130c4480
etdofresh@gmail.com <>
2023-10-09 18:25:55 -0500

    Update command to help the plastic sync work better.

--- cm-sync-git.cmd
+++ cm-sync-git.cmd
@@ -1,2 +1,4 @@
-cm sync rep:UDevelop@etdofresh_unity@cloud git https://github.com/ETdoFresh/UDevelop
+set CM_SYNC_USER=ETdoFresh@gmail.com
+for /F "tokens=*" %%g IN ('gh auth token') do (SET CM_SYNC_TOKEN=%%g)
+cm sync rep:UDevelop@etdofresh_unity@cloud git https://github.com/ETdoFresh/UDevelop --user=%CM_SYNC_USER% --pwd=%CM_SYNC_TOKEN%
 pause
