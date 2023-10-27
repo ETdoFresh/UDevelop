7b35dda456018d47d6c2aa6c48d597ea9be55bde
ETdoFresh <ETdoFresh@gmail.com>
2023-10-27 08:24:18 -0500

    Add PackageCommand.cs.

---
+++ Assets/CommandSystem/Commands/PackageCommand.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEditor.PackageManager.Requests;
+
+namespace CommandSystem.Commands
+{
+    [Serializable]
+    public class PackageCommand : Command
+    {
+        private static ListRequest _packageListRequest;
+
+        private string _commandOutput;
+
+        public override string CommandOutput => _commandOutput;
+
+        public PackageCommand(string commandInput) : base(commandInput) { }
+
+        public override void OnRun(params string[] args)
+        {
+            var action = args[1];
+
+            switch (action)
+            {
+                case "add":
+                    var packageName = args[2];
+                    UnityEditor.PackageManager.Client.Add(packageName);
+                    _commandOutput = $"Added package {packageName}";
+                    break;
+                case "remove":
+                    var packageName2 = args[2];
+                    UnityEditor.PackageManager.Client.Remove(packageName2);
+                    _commandOutput = $"Removed package {packageName2}";
+                    break;
+                case "list":
+                    if (_packageListRequest == null)
+                    {
+                        _packageListRequest = UnityEditor.PackageManager.Client.List();
+                        _commandOutput = "Loading... Try again in a few seconds.";
+                    }
+                    else if (!_packageListRequest.IsCompleted)
+                    {
+                        _commandOutput = "Still loading... Try again in a few seconds.";
+                    }
+                    else
+                    {
+                        _commandOutput = "";
+                        foreach (var package in _packageListRequest.Result)
+                            _commandOutput += $"{package.name} - {package.version}\n";
+                    }
+                    break;
+                default:
+                    throw new Exception($"Package action {action} not found!");
+            }
+        }
+    }
+}
