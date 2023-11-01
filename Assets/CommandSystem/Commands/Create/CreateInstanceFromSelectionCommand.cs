171dc66de0603e0b4f452803ddeb8a5ec4d96bdb
ETdoFresh <ETdoFresh@gmail.com>
2023-11-01 08:35:18 -0500

    Start to work on the next iteration of commands. A json file that contains all steps to run command.

---
+++ Assets/CommandSystem/Commands/Create/CreateInstanceFromSelectionCommand.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+namespace CommandSystem.Commands.Create
+{
+    [Serializable]
+    public class CreateInstanceFromSelectionCommand : Command
+    {
+        private GameObject[] _prefabs;
+        private GameObject[] _instances;
+
+        public CreateInstanceFromSelectionCommand(string commandInput) : base(commandInput) { }
+
+        public override void OnRun(params string[] args)
+        {
+#if UNITY_EDITOR
+            var selectedGameObjects = UnityEditor.Selection.gameObjects;
+            _prefabs = selectedGameObjects
+                .Where(UnityEditor.PrefabUtility.IsAnyPrefabInstanceRoot).ToArray();
+
+            _instances = new GameObject[_prefabs.Length];
+            for (var i = 0; i < _prefabs.Length; i++)
+            {
+                var prefab = _prefabs[i];
+                var instance = UnityEditor.PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+                instance.name = prefab.name;
+                _instances[i] = instance;
+            }
+            UnityEditor.Selection.objects = _instances;
+#else
+            throw new InvalidOperationException("Create Object in Project Commands only work in Unity Editor!");
+#endif
+        }
+
+        public override void OnUndo()
+        {
+#if UNITY_EDITOR
+            foreach (var instance in _instances) 
+                UnityEngine.Object.DestroyImmediate(instance);
+            _instances = null;
+            UnityEditor.Selection.objects = _prefabs;
+#endif
+        }
+
+        public override void OnRedo()
+        {
+#if UNITY_EDITOR
+            _instances = new GameObject[_prefabs.Length];
+            for (var i = 0; i < _prefabs.Length; i++)
+            {
+                var prefab = _prefabs[i];
+                var instance = UnityEditor.PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+                instance.name = prefab.name;
+                _instances[i] = instance;
+            }
+            UnityEditor.Selection.objects = _instances;
+#endif
+        }
+    }
+}
--- Assets/CommandSystem/Commands/Create/CreatePrefabCommand.cs
+++ Assets/CommandSystem/Commands/Create/CreatePrefabCommand.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using UnityEngine;
 
 namespace CommandSystem.Commands.Create
 {
@@ -10,20 +11,13 @@ namespace CommandSystem.Commands.Create
 
         public CreatePrefabCommand(string commandInput) : base(commandInput) { }
 
-        public override bool AddToHistory => true;
-        public override string CommandOutput => $"Created Prefab {_prefabName}";
-
-        public override string[] CommandAliases => new[] { "create-prefab", "createprefab", "c-p" };
-        public override string CommandUsage => $"{CommandAliases[0]} [PREFAB_NAME/PATH]";
-        public override string CommandDescription => "Creates an empty .prefab object in project.";
-
         public override void OnRun(params string[] args)
         {
 #if UNITY_EDITOR
-            var prefabNameOrPath =  args.Length < 2 ? null : string.Join("_", args[1..]);
+            var prefabNameOrPath = args.Length < 2 ? null : string.Join("_", args[1..]);
             _prefabPath = CommandAsset.ResolvePath(prefabNameOrPath, "Prefab", ".prefab");
             _prefabName = CommandAsset.GetNameFromPath(_prefabPath);
-            var emptyGameObject = new UnityEngine.GameObject(_prefabName);
+            var emptyGameObject = new GameObject(_prefabName);
             var prefab = UnityEditor.PrefabUtility.SaveAsPrefabAsset(emptyGameObject, _prefabPath);
             UnityEngine.Object.DestroyImmediate(emptyGameObject);
             UnityEditor.AssetDatabase.SaveAssets();
@@ -44,7 +38,7 @@ namespace CommandSystem.Commands.Create
         public override void OnRedo()
         {
 #if UNITY_EDITOR
-            var emptyGameObject = new UnityEngine.GameObject(_prefabName);
+            var emptyGameObject = new GameObject(_prefabName);
             var prefab = UnityEditor.PrefabUtility.SaveAsPrefabAsset(emptyGameObject, _prefabPath);
             UnityEngine.Object.DestroyImmediate(emptyGameObject);
             UnityEditor.AssetDatabase.SaveAssets();
