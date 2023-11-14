c2436a62fdfbe620c88175d78236168a9a5a4851
ETdoFresh <ETdoFresh@gmail.com>
2023-11-13 21:48:47 -0600

    Move files around. Clean up directory structure a little bit.

--- Assets/CommandSystem/CommandArgs.cs
+++ Assets/CommandSystem/CommandArgs.cs
@@ -1,28 +1,10 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace CommandSystem.Commands
 {
     public static class CommandArgs
     {
-        public static string GetArgByAlias(Type commandType, int arg, string alias)
-        {
-            var aliases =
-                CommandJsonData.GetKeyAndValue<string[]>($"{commandType.Name}.Arg{arg}.PossibleValues", "Aliases");
-            
-            if (aliases == null) return null;
-            
-            foreach (var entry in aliases)
-            {
-                if (entry.Value == null) continue;
-                if (entry.Value.Contains(alias))
-                    return entry.Key;
-            }
-            
-            return null;
-        }
-        
         public static Dictionary<string, string[]> GetArgAliases(Type commandType, int arg)
         {
             return CommandJsonData.GetKeyAndValue<string[]>($"{commandType.Name}.Arg{arg}.PossibleValues", "Aliases");
