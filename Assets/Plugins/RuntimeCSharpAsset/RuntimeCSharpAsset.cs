6c512e2026536bfe6c7339b5e3a65388e7d52762
ETdoFresh <ETdoFresh@gmail.com>
2023-11-28 23:44:55 -0600

    Test changing cube code in a build.

--- Assets/Plugins/RuntimeCSharpAsset/RuntimeCSharpAsset.cs
+++ Assets/Plugins/RuntimeCSharpAsset/RuntimeCSharpAsset.cs
@@ -1,15 +1,18 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Events;
 
 public class RuntimeCSharpAsset : ScriptableObject
 {
+    private const string PersistentDataPath = "CSharpSourceCode";
+    
     [SerializeField] public string filePath;
     [SerializeField, TextArea(3, 50)] public string sourceCode;
     [SerializeField] private UnityEvent sourceChanged = new();
     private string _previousSourceCode;
-    
+
     public UnityEvent SourceChanged => sourceChanged;
-    
+
     public void OnValidate()
     {
 #if UNITY_EDITOR
@@ -19,9 +22,33 @@ public class RuntimeCSharpAsset : ScriptableObject
 #endif
     }
 
+    private void Awake()
+    {
+#if !UNITY_EDITOR
+        var filename = System.IO.Path.GetFileName(filePath);
+        var path = System.IO.Path.Combine(Application.persistentDataPath, PersistentDataPath, filename);
+        if (System.IO.File.Exists(path) == false) return;
+        sourceCode = System.IO.File.ReadAllText(path);
+        Debug.Log($"Loaded from {path}");
+#endif
+    }
+
     public void Save()
     {
+#if UNITY_EDITOR
         System.IO.File.WriteAllText(filePath, sourceCode);
+#else
+        var filename = System.IO.Path.GetFileName(filePath);
+        // Save to persistent data path
+        var directory = System.IO.Path.Combine(Application.persistentDataPath, PersistentDataPath);
+        if (System.IO.Directory.Exists(directory) == false)
+        {
+            System.IO.Directory.CreateDirectory(directory);
+        }
+        var path = System.IO.Path.Combine(directory, filename);
+        System.IO.File.WriteAllText(path, sourceCode);
+        Debug.Log($"Saved to {path}");
+#endif
         sourceChanged.Invoke();
     }
 }
